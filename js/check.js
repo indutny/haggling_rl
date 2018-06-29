c5bf4ed140a7b37d1e5edcd92df832d78572fcd8
Fedor Indutny <fedor@indutny.com>
2018-06-29 18:54:15 -0400

    save

--- js/check.js
+++ js/check.js
@@ -8,5 +8,4 @@ const log = (msg) => {
 
 const a = new Agent(null, [ 2, 2, 1 ], [ 1, 3, 1 ], 5, log);
 
-console.log(a.offer([ 1, 1, 0 ]));
-console.log(a.offer([ 2, 1, 0 ]));
+console.log(a.offer());
