ff520eb7d06eaf4d0e8365c1a0b5bcf9a90906dc
Fedor Indutny <fedor@indutny.com>
2018-07-21 14:13:24 -0400

    restore-15200_sing-27500

--- js/eval.js
+++ js/eval.js
@@ -118,8 +118,8 @@ function addContestant(name, A) {
   });
 }
 
-addContestant('neural', Neural);
-// addContestant('neural07', Neural07);
+// addContestant('neural', Neural);
+addContestant('neural07', Neural07);
 // addContestant('neural_b', NeuralB);
 // addContestant('best', BestNeural);
 
